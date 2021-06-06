41fa29092eec3eeb1087784d34e26c378849fa98
WenZheng Li <me@wenzheng.li>
2021-06-07 08:06:14 +0900

    integrated MPU6050 component

---
+++ firmware/esp32_app/main/main.cpp
@@ -0,0 +1,130 @@
+/* Hello World Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+
+#include <stdio.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_spi_flash.h"
+
+#include <driver/i2c.h>
+#include <esp_log.h>
+#include <esp_err.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+
+#include "MPU6050.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+#include "sdkconfig.h"
+
+#define PIN_SDA 22
+#define PIN_CLK 21
+
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+uint16_t packetSize = 42;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+
+void task_initI2C(void *ignore) {
+    i2c_config_t conf;
+    conf.mode = I2C_MODE_MASTER;
+    conf.sda_io_num = (gpio_num_t)PIN_SDA;
+    conf.scl_io_num = (gpio_num_t)PIN_CLK;
+    conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
+    conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
+    conf.master.clk_speed = 400000;
+    ESP_ERROR_CHECK(i2c_param_config(I2C_NUM_0, &conf));
+    ESP_ERROR_CHECK(i2c_driver_install(I2C_NUM_0, I2C_MODE_MASTER, 0, 0, 0));
+    vTaskDelete(NULL);
+}
+
+void task_display(void*){
+    MPU6050 mpu = MPU6050();
+    mpu.initialize();
+    mpu.dmpInitialize();
+
+    // This need to be setup individually
+    mpu.setXGyroOffset(220);
+    mpu.setYGyroOffset(76);
+    mpu.setZGyroOffset(-85);
+    mpu.setZAccelOffset(1788);
+
+    mpu.setDMPEnabled(true);
+
+    while(1){
+        mpuIntStatus = mpu.getIntStatus();
+        // get current FIFO count
+        fifoCount = mpu.getFIFOCount();
+
+        if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+            // reset so we can continue cleanly
+            mpu.resetFIFO();
+
+            // otherwise, check for DMP data ready interrupt frequently)
+        } else if (mpuIntStatus & 0x02) {
+            // wait for correct available data length, should be a VERY short wait
+            while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+
+            // read a packet from FIFO
+
+            mpu.getFIFOBytes(fifoBuffer, packetSize);
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            mpu.dmpGetGravity(&gravity, &q);
+            mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+            printf("YAW: %3.1f, ", ypr[0] * 180/M_PI);
+            printf("PITCH: %3.1f, ", ypr[1] * 180/M_PI);
+            printf("ROLL: %3.1f \n", ypr[2] * 180/M_PI);
+        }
+
+        //Best result is to match with DMP refresh rate
+        // Its last value in components/MPU6050/MPU6050_6Axis_MotionApps20.h file line 310
+        // Now its 0x13, which means DMP is refreshed with 10Hz rate
+        vTaskDelay(100/portTICK_PERIOD_MS);
+    }
+
+    vTaskDelete(NULL);
+}
+
+extern "C"
+{
+
+void app_main(void)
+{
+    printf("Hello world!\n");
+
+    /* Print chip information */
+    esp_chip_info_t chip_info;
+    esp_chip_info(&chip_info);
+    printf("This is %s chip with %d CPU core(s), WiFi%s%s, ",
+            CONFIG_IDF_TARGET,
+            chip_info.cores,
+            (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
+            (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
+
+    printf("silicon revision %d, ", chip_info.revision);
+
+    printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
+            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+
+    printf("Minimum free heap size: %d bytes\n", esp_get_minimum_free_heap_size());
+
+    for (int i = 10; i >= 0; i--) {
+        printf("Restarting in %d seconds...\n", i);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+    printf("Restarting now.\n");
+    fflush(stdout);
+    esp_restart();
+}
+
+}
--- firmware/idf.sh
+++ firmware/idf.sh
@@ -1,4 +1,8 @@
 #/bin/bash
 version=release-v4.2
 project=esp32_app
-docker run -it --rm --privileged -v $PWD/$project:/project -w /project espressif/idf:$version idf.py $@
+docker run -it --rm --privileged -w /project -v $PWD/$project:/project \
+-v $PWD/../submodules/i2cdevlib/ESP32_ESP-IDF/components/I2Cdev:/project/components/I2Cdev \
+-v $PWD/../submodules/i2cdevlib/ESP32_ESP-IDF/components/MPU6050:/project/components/MPU6050 \
+espressif/idf:$version idf.py $@
+
