a4a488b364826f23f4eb864f3e0a7a5681efa4ae
WenZheng Li <me@wenzheng.li>
2021-08-01 03:45:25 +0900

    changed docker build script path to stem from script dir

--- firmware/idf.sh
+++ firmware/idf.sh
@@ -1,8 +1,9 @@
 #/bin/bash
 docker_image=espressif/idf:release-v4.3
 app=esp32_app
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 docker run -it --rm --privileged -w /project/firmware/app \
--v $PWD/$app:/project/firmware/app \
--v $PWD/../submodules:/project/submodules \
--v $PWD/../libslab:/project/libslab \
+-v $SCRIPT_DIR/$app:/project/firmware/app \
+-v $SCRIPT_DIR/../submodules:/project/submodules \
+-v $SCRIPT_DIR/../libslab:/project/libslab \
 $docker_image idf.py $@
