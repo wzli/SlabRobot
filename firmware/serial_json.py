fb335a4ccff503feeafe96cfbe6dafae3ba20eb6
WenZheng Li <me@wenzheng.li>
2021-08-03 09:00:09 +0900

    added gui tree view to serial json

--- firmware/serial_json.py
+++ firmware/serial_json.py
@@ -1,22 +1,65 @@
 #!/usr/bin/env python3
 
 import argparse
+import json
 import serial
-import yaml
+import threading
+import tkinter as tk
+from tkinter import ttk
+
+
+def json_tree_update(tree, parent, dictionary):
+    for key, value in dictionary.items():
+        node = "/".join((parent, str(key)))
+        if isinstance(value, list):
+            value = dict(enumerate(value))
+        try:
+            tree.insert(parent, "end", node, text=key)
+        except tk.TclError:
+            pass
+        try:
+            json_tree_update(tree, node, value)
+        except AttributeError:
+            tree.set(node, 0, value)
+
+
+class SerialJson:
+    def __init__(self, port, baudrate):
+        self.port = port
+        self.baudrate = baudrate
+        self.latest_data = None
+        self.root = tk.Tk()
+        self.root.title("Live Data")
+        self.root.bind("<Escape>", lambda x: self.root.destroy())
+        self.tree = ttk.Treeview(self.root, columns=0)
+        self.tree.pack(expand=1, fill=tk.BOTH)
+
+    def run(self):
+        self.serial_thread = threading.Thread(
+            target=self.poll_serial, daemon=True
+        ).start()
+        self.gui_update()
+        self.root.mainloop()
+
+    def poll_serial(self):
+        with serial.Serial(self.port, self.baudrate) as ser:
+            while True:
+                line_bytes = ser.readline()
+                line_string = line_bytes.decode("ascii", "ignore")
+                try:
+                    self.latest_data = json.loads(line_string)
+                except:
+                    print(line_string, end="")
+
+    def gui_update(self):
+        self.root.after(10, self.gui_update)
+        if self.latest_data:
+            json_tree_update(self.tree, "", self.latest_data)
+            self.latest_data = None
+
 
 parser = argparse.ArgumentParser(description="print serial json stream")
 parser.add_argument("-p", "--port", type=str, default="/dev/ttyUSB0")
 parser.add_argument("-b", "--baudrate", type=int, default=115200)
 args = parser.parse_args()
-
-with serial.Serial(args.port, args.baudrate) as ser:
-    while True:
-        line_bytes = ser.readline()
-        line_string = line_bytes.decode("ascii", "ignore")
-        try:
-            obj = yaml.safe_load(line_string)
-            assert obj
-            line_string = yaml.safe_dump(obj, sort_keys=False)
-        except:
-            pass
-        print(line_string, end="")
+SerialJson(port=args.port, baudrate=args.baudrate).run()
