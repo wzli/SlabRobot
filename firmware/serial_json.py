f391fca174c3370a08d0fd61b06f2ef88f41c37e
WenZheng Li <me@wenzheng.li>
2021-08-03 22:20:54 +0900

    added serial json seperate message timestamps

--- firmware/serial_json.py
+++ firmware/serial_json.py
@@ -3,6 +3,7 @@
 import argparse
 import json
 import serial
+import time
 import threading
 import tkinter as tk
 from tkinter import ttk
@@ -29,9 +30,13 @@ class SerialJson:
     def __init__(self, port, baudrate):
         self.port = port
         self.baudrate = baudrate
-        self.latest_data = None
+        self.latest_data = {}
+        self.start_time = time.monotonic()
+        self.latest_time = 0
+        self.timestamps = {}
         self.root = tk.Tk()
         self.root.title("Live Data")
+        self.root.geometry("400x800")
         self.root.bind("<Escape>", lambda x: self.root.destroy())
         self.tree = ttk.Treeview(self.root, columns=0)
         self.tree.pack(expand=1, fill=tk.BOTH)
@@ -49,15 +54,25 @@ class SerialJson:
                 line_bytes = ser.readline()
                 line_string = line_bytes.decode("ascii", "ignore")
                 try:
-                    self.latest_data = json.loads(line_string)
+                    data = json.loads(line_string)
+                    self.latest_data.update(data)
+                    self.latest_time = time.monotonic()
+                    self.timestamps.update({key: self.latest_time for key in data})
                 except:
                     print(line_string, end="")
 
     def gui_update(self):
-        self.root.after(10, self.gui_update)
-        if self.latest_data:
-            json_tree_update(self.tree, "", self.latest_data)
-            self.latest_data = None
+        self.root.after(5, self.gui_update)
+        for key, value in self.latest_data.items():
+            if self.timestamps[key] != self.latest_time:
+                continue
+            try:
+                self.tree.insert("", "end", key, text=key, open=True)
+            except tk.TclError:
+                pass
+            self.tree.set(key, 0, round(self.latest_time - self.start_time, 6))
+            json_tree_update(self.tree, key, value)
+        self.latest_time = time.monotonic()
 
 
 parser = argparse.ArgumentParser(description="print serial json stream")
